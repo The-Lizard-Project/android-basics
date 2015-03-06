db1b676cd55de6a1df315d4c9b2e34e49547be33
Mateusz Boś <mateuszbos@gmail.com>
2015-03-06 15:07:17 +0100

    Added a branch for OpenShift hsoting

--- Backend/runserver.bat
+++
@@ -1,4 +0,0 @@
-cd flags
-run 00mongoose-free-5.5.exe &
-cd ..
-python manage.py runserver 0.0.0.0:8086
--- Code/app/src/main/java/basic/android/fp/pl/androidbasic/util/FlagAddress.java
+++
@@ -1,8 +0,0 @@
-package basic.android.fp.pl.androidbasic.util;
-
-public class FlagAddress {
-
-	public static String obtainAddress(Currency currency) {
-		return "http://www.currencysymbols.in/flags/" + currency.getCountry().toLowerCase().replace(" ", "") + ".png";
-	}
-}
