2eb53664d42b5fcfacb37abe07856b112bb526bf
Mateusz Boś <mateuszbos@gmail.com>
2015-03-06 02:23:38 +0100

    Corrected version of WS

--- Backend/AndroidBackend/views.py
+++ Backend/AndroidBackend/views.py
@@ -1,24 +1,121 @@
-from django.shortcuts import render
 import json
 from django.http import HttpResponse
 import urllib2
 
+
 # Create your views here.
 def list_of_exchange_rates(request, iso_code):
-    data = ""
+    data = {}
     try:
         jsonRequest = urllib2.urlopen("http://api.fixer.io/latest?base=" + iso_code)
         content = jsonRequest.read()
-        data = json.loads(content.decode("utf8"))
-    except Exception:
-        data = {"base": iso_code, "date": "2015-03-05",
-                "rates": {"AUD": 0.3431, "BGN": 0.4724, "BRL": 0.7974, "CAD": 0.3326, "CHF": 0.2584,
-                          "CNY": 1.676, "CZK": 6.6242, "DKK": 1.8007, "GBP": 0.1752, "HKD": 2.0737,
-                          "HRK": 1.85, "HUF": 73.764, "IDR": 3469.59, "ILS": 1.0694, "INR": 16.646,
-                          "JPY": 32.152, "KRW": 294.43, "MXN": 4.0236, "MYR": 0.9763, "NOK": 2.0644,
-                          "NZD": 0.357, "PHP": 11.8, "RON": 1.0738, "RUB": 16.332, "SEK": 2.2258,
-                          "SGD": 0.3661, "THB": 8.6685, "TRY": 0.6924, "USD": 0.2674, "ZAR": 3.1443,
-                          "EUR": 0.2416},
-                "error": "Returned default values due to an error"}
+        tmpData = json.loads(content.decode("utf8"))
+        data['date'] = tmpData['date']
+        data['base'] = tmpData['base']
+        mmr = tmpData['rates']
+        rate_recs = []
+        for key, value in mmr.iteritems():
+            rate_recs.append({'currency': key, 'rate': value})
+        data['rates'] = rate_recs
+    except Exception as detail:
+        data = {
+            "date": "2015-03-05",
+            "base": "PLN",
+            "rates": [{
+                          "currency": "USD",
+                          "rate": 0.2674
+                      }, {
+                          "currency": "IDR",
+                          "rate": 3469.59
+                      }, {
+                          "currency": "BGN",
+                          "rate": 0.4724
+                      }, {
+                          "currency": "ILS",
+                          "rate": 1.0694
+                      }, {
+                          "currency": "GBP",
+                          "rate": 0.1752
+                      }, {
+                          "currency": "DKK",
+                          "rate": 1.8007
+                      }, {
+                          "currency": "CAD",
+                          "rate": 0.3326
+                      }, {
+                          "currency": "JPY",
+                          "rate": 32.152
+                      }, {
+                          "currency": "HUF",
+                          "rate": 73.764
+                      }, {
+                          "currency": "RON",
+                          "rate": 1.0738
+                      }, {
+                          "currency": "MYR",
+                          "rate": 0.9763
+                      }, {
+                          "currency": "SEK",
+                          "rate": 2.2258
+                      }, {
+                          "currency": "SGD",
+                          "rate": 0.3661
+                      }, {
+                          "currency": "HKD",
+                          "rate": 2.0737
+                      }, {
+                          "currency": "AUD",
+                          "rate": 0.3431
+                      }, {
+                          "currency": "CHF",
+                          "rate": 0.2584
+                      }, {
+                          "currency": "KRW",
+                          "rate": 294.43
+                      }, {
+                          "currency": "CNY",
+                          "rate": 1.676
+                      }, {
+                          "currency": "TRY",
+                          "rate": 0.6924
+                      }, {
+                          "currency": "HRK",
+                          "rate": 1.85
+                      }, {
+                          "currency": "NZD",
+                          "rate": 0.357
+                      }, {
+                          "currency": "THB",
+                          "rate": 8.6685
+                      }, {
+                          "currency": "EUR",
+                          "rate": 0.2416
+                      }, {
+                          "currency": "NOK",
+                          "rate": 2.0644
+                      }, {
+                          "currency": "RUB",
+                          "rate": 16.332
+                      }, {
+                          "currency": "INR",
+                          "rate": 16.646
+                      }, {
+                          "currency": "MXN",
+                          "rate": 4.0236
+                      }, {
+                          "currency": "CZK",
+                          "rate": 6.6242
+                      }, {
+                          "currency": "BRL",
+                          "rate": 0.7974
+                      }, {
+                          "currency": "PHP",
+                          "rate": 11.8
+                      }, {
+                          "currency": "ZAR",
+                          "rate": 3.1443
+                      }
+            ],
+            "error": detail}
 
     return HttpResponse(json.dumps(data), content_type="application/json")
--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import patterns, url
-import settings
 
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'Backend.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-                       # url(r'^admin/', include(admin.site.urls)),
 
                        url(r'^list/(?P<iso_code>[a-zA-Z]{3})$',
-                           'AndroidBackend.views.list_of_exchange_rates'),
-                       url(r'^flags/(?P<path>.*)$', 'django.views.static.serve', {
-                           'document_root': settings.MEDIA_ROOT}))
+                           'AndroidBackend.views.list_of_exchange_rates'))
