19939e4dba0638d11a89835f39e12ffb604e79f2
Michał Górski <mgorski@future-processing.com>
2015-03-07 09:12:48 +0100

    added code to static content server

--- Backend/static_content/flags/src/FlagAddress.java
+++
@@ -1,8 +0,0 @@
-package basic.android.fp.pl.androidbasic.util;
-
-public class FlagAddress {
-
-	public static String obtainAddress(Currency currency) {
-		return "http://192.168.0.11:8087/" + currency.getCountry().toLowerCase().replace(" ", "") + ".png";
-	}
-}
