a4171a71947dc3b482e9965e0b126a5f74ce9c05
Mateusz Boś <mateuszbos@gmail.com>
2015-11-11 20:47:17 +0100

    Made django serve static data

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import patterns, url
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'Backend.views.home', name='home'),
 
                        url(r'^list/(?P<iso_code>[a-zA-Z]{3})$',
-                           'AndroidBackend.views.list_of_exchange_rates'))
+                           'AndroidBackend.views.list_of_exchange_rates'),
+			
+			url(r'^favicon.ico$',RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'),permanent=False),
+					     name="favicon"
+			)	
+		)
+
+	
